89618c66e4ade98cea44ec5b14c3eaab67452478
Vladyslav Tolosiuk <vtolosiuk@omilia.com>
2023-12-30 21:22:34 +0200

    added greeting

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Natashock!! I have made this website for you !!
         </p>
         <a
           className="App-link"
